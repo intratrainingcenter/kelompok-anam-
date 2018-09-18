b76fa84e21068903bd86b8348c960cc273a19654
Khoirul Anam <namanam38@gmail.com>
2018-09-18 15:48:13 +0700

    coba coba

--- config/app.php
+++ config/app.php
@@ -146,7 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Collective\html\htmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
         /*
          * Package Service Providers...
          */
